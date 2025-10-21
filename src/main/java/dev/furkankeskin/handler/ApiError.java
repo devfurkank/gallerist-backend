6b96b3f540f9c6de839f6db401f5569a5afc557f
Furkan Keskin <keskin134@gmail.com>
2025-10-21 11:28:45 +0300

    Add global exception handling with ApiError and Exception classes

---
+++ src/main/java/dev/furkankeskin/handler/ApiError.java
@@ -0,0 +1,15 @@
+package dev.furkankeskin.handler;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiError<E> {
+
+    private Integer status;
+
+    private Exception<E> exception;
+}
